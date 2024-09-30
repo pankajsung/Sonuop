17fc6914a3913b6f3e077905a568343abf79039b
pankajsung <singhshab8181@gmail.com>
2024-09-30 16:29:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7128147433:AAHmeS7-dOCmqEY-sciAylQp5pe2kNrbA-Y")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("ILLEGALCHEAT78")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6697302838")
+
+
+
+
+
+
+
+  
